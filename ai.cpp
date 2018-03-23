d8d0a4eeb6e44fef2ebc521e5a7f8eecee565b85
E34042054 <e34042054@mail.ncku.edu.tw>
2018-03-24 00:14:21 +0800

    aa

--- ai.cpp
+++ ai.cpp
@@ -49,111 +49,111 @@ int main() {
 
     cout<<"9 6 3 7 5 2 4 8"<<endl;
 
-  //  while(1) {
+    //  while(1) {
 
-        while(1) {
+    while(1) {
 
-            string input,tmp;
+        string input,tmp;
 
-            getline(cin,input);
+        getline(cin,input);
 
-            if(input == "END") {
-                break;
-            }
+        if(input == "END") {
+            break;
+        }
 
 
-            stringstream ss(input);
+        stringstream ss(input);
 
+        ss>>tmp;
+
+        if(tmp == "TIME") {
+            ss>>time;
+        }
+        if(tmp == "MANA") {
+            ss>>mana;
+        }
+
+        if(tmp == "DECK") {
+            ss>>deck1;
+
+
+            ss>>deck2;
+            ss>>deck3;
+            ss>>deck4;
+        }
+
+        if(tmp == "TOWER") {
+            ss>>tower_num;
+            ss>>tower[tower_num];
+        }
+        if(tmp == "FRIEND") {
+            ss>>all[frinum].id;
             ss>>tmp;
-
-            if(tmp == "TIME") {
-                ss>>time;
-            }
-            if(tmp == "MANA") {
-                ss>>mana;
-            }
-
-            if(tmp == "DECK") {
-                ss>>deck1;
-
-
-                ss>>deck2;
-                ss>>deck3;
-                ss>>deck4;
-            }
-
-            if(tmp == "TOWER") {
-                ss>>tower_num;
-                ss>>tower[tower_num];
-            }
-            if(tmp == "FRIEND") {
-                ss>>all[frinum].id;
-                ss>>tmp;
-                ss>>all[frinum].pos_x;
-                ss>>period;
-                ss>>all[frinum].pos_y;
-                ss>>tmp;
-                ss>>all[frinum].HP;
-                frinum++;
-            }
-
-            if(tmp == "ENEMY") {
-                ss>>ene[enenum].id;
-                ss>>tmp;
-                ss>>ene[enenum].pos_x;
-                ss>>period;
-                ss>>ene[enenum].pos_y;
-                ss>>tmp;
-                ss>>ene[enenum].HP;
-                enenum++;
-            }
-
+            ss>>all[frinum].pos_x;
+            ss>>period;
+            ss>>all[frinum].pos_y;
+            ss>>tmp;
+            ss>>all[frinum].HP;
+            frinum++;
         }
 
- /*       if(mana>=2) {
-            cout<<"1 3 5 23"<<endl<<0;
-        }
-        if(mana>=2) {
-            cout<<"1 3 15 23"<<endl<<0;
-        }
-
-        if(mana>=3) {
-            cout<<"1 9 5 12"<<endl<<0;
-        }
-
-        if(mana>=1) {
-            cout<<"1 9 5 10"<<endl<<0;
+        if(tmp == "ENEMY") {
+            ss>>ene[enenum].id;
+            ss>>tmp;
+            ss>>ene[enenum].pos_x;
+            ss>>period;
+            ss>>ene[enenum].pos_y;
+            ss>>tmp;
+            ss>>ene[enenum].HP;
+            enenum++;
         }
 
-        if(mana>=4) {
-            cout<<"1 6 5 20"<<endl<<0;
-        }
-
-        if(mana>=4) {
-            cout<<"1 6 15 20"<<endl<<0;
-        }
-
-        if(mana>=2&&mana<8) {
-            cout<<"1 3 15 23"<<endl<<0;
-        }
-
-        else {
-            cout<<"1 7 15 23"<<endl<<0;
-        }
-
-        if(mana>=2&&mana<8) {
-            cout<<"1 3 5 23"<<endl<<0;
-        }
-
-        else {
-            cout<<"1 7 5 23"<<endl<<0;
-        }
     }
-    //	 if(tower[6]!=8000){
 
-    //  cout<<"1 8 15 12";}
+    /*       if(mana>=2) {
+               cout<<"1 3 5 23"<<endl<<0;
+           }
+           if(mana>=2) {
+               cout<<"1 3 15 23"<<endl<<0;
+           }
 
-*/
+           if(mana>=3) {
+               cout<<"1 9 5 12"<<endl<<0;
+           }
+
+           if(mana>=1) {
+               cout<<"1 9 5 10"<<endl<<0;
+           }
+
+           if(mana>=4) {
+               cout<<"1 6 5 20"<<endl<<0;
+           }
+
+           if(mana>=4) {
+               cout<<"1 6 15 20"<<endl<<0;
+           }
+
+           if(mana>=2&&mana<8) {
+               cout<<"1 3 15 23"<<endl<<0;
+           }
+
+           else {
+               cout<<"1 7 15 23"<<endl<<0;
+           }
+
+           if(mana>=2&&mana<8) {
+               cout<<"1 3 5 23"<<endl<<0;
+           }
+
+           else {
+               cout<<"1 7 5 23"<<endl<<0;
+           }
+       }
+       //	 if(tower[6]!=8000){
+
+       //  cout<<"1 8 15 12";}
+
+    */
 
     //  cout<<tower[1]<<" "<<tower[2]<<endl;
 //	cout<<all[0].id<<" "<<all[0].pos_x<<" "<<all[0].pos_y<<" "<<all[0].HP<<endl;
